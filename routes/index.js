9b161de1691083dfb37315c949fc52d61ff4e200
Bimal Kumar <40321@hexaware.com>
2018-02-20 12:29:36 +0530

    changed method typr

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const routes = require('express').Router();
 var controllers = require('../controllers/api');
 
 routes.post('/incident/create', controllers.createIncident);
-routes.get('/incident/getstatus', controllers.getIncidentStatus);
+routes.post('/incident/getstatus', controllers.getIncidentStatus);
 
 module.exports = routes;
