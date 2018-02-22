4475122c208c56ad6875b417688a32ce77cdd3ea
Bimal Kumar <40321@hexaware.com>
2018-02-22 10:24:38 +0530

    added webhook verification asn weebhook endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 var controllers = require('../controllers/api');
 
-routes.get('/', controllers.renderHome);
+routes.post('/webhook', controllers.webhookEndpoint);
+routes.get('/webhook', controllers.webhookVerification);
 routes.post('/webhook/processIncident', controllers.processIncident);
 
 module.exports = routes;
