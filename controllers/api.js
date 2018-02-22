cc2d92fc4c3dd9beefbcc472c693556423f297eb
Bimal Kumar <40321@hexaware.com>
2018-02-22 12:16:26 +0530

    bug fix-code block mismatch

--- controllers/api.js
+++ controllers/api.js
@@ -31,16 +31,16 @@ var self = {
 				// Get the sender PSID
 				let sender_psid = webhook_event.sender.id;
 				console.log('Sender PSID: ' + sender_psid);
+
+				// Check if the event is a message or postback and
+				// pass the event to the appropriate handler function
+				if (webhook_event.message) {
+					self.handleMessage(sender_psid, webhook_event.message);
+				} else if (webhook_event.postback) {
+					self.handlePostback(sender_psid, webhook_event.postback);
+				}
 			});
-
-			// Check if the event is a message or postback and
-			// pass the event to the appropriate handler function
-			if (webhook_event.message) {
-				self.handleMessage(sender_psid, webhook_event.message);
-			} else if (webhook_event.postback) {
-				self.handlePostback(sender_psid, webhook_event.postback);
-			}
-
+			
 			// Returns a '200 OK' response to all requests
 			res.status(200).send('EVENT_RECEIVED');
 		} else {
