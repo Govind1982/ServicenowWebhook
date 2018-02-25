5601e995e63963efbf492b4fac12a0e61058e122
unknown <bimalkumar@gmail.com>
2018-02-25 18:21:45 +0530

    addedhelpers

---
+++ helpers/dialogflow.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var self = {
+    invokeCreateIncidentEvent: function (request_body) {
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": "https://api.dialogflow.com/v1/query?v=20150910&e=create_incident_event&timezone=Europe/Paris&lang=en&sessionId=1234567890",
+            "method": "GET",
+            "headers": {
+                'Authorization': 'Bearer ' + process.env.DIALOGFLOWAGENT_CLIENT_ACCESS_TOKEN
+            },
+        }, (err, res, body) => {
+            if (!err) {
+                console.log(res.body);
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        });
+    }
+}
+
+module.exports = self;
