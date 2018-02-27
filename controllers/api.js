f29e7303d94445d5420d8eb461906fce7c57ea83
bimalkumarh <bimalkumarh@gmail.com>
2018-02-27 09:42:46 +0530

    aded new library for facebook bot integration

--- controllers/api.js
+++ controllers/api.js
@@ -1,11 +1,78 @@
 'use strict';
 
+require('../libraries/FacebookBot');
 var apiai = require('apiai');
 const request = require('request');
 const GlideRecord = require('servicenow-rest').gliderecord;
 const gr = new GlideRecord(process.env.SERVICENOW_INSTANCE, process.env.SERVICENOW_TABLE, process.env.SERVICENOW_USERNAME, process.env.SERVICENOW_PASSWORD, process.env.SERVICENOW_API_VERSION);
+let facebookBot = new FacebookBot();
 
 var self = {
+	webhookEndpoint: function (req, res) {
+		try {
+			const data = JSONbig.parse(req.body);
+
+			if (data.entry) {
+				let entries = data.entry;
+				entries.forEach((entry) => {
+					let messaging_events = entry.messaging;
+					if (messaging_events) {
+						messaging_events.forEach((event) => {
+							if (event.message && !event.message.is_echo) {
+
+								if (event.message.attachments) {
+									let locations = event.message.attachments.filter(a => a.type === "location");
+
+									// delete all locations from original message
+									event.message.attachments = event.message.attachments.filter(a => a.type !== "location");
+
+									if (locations.length > 0) {
+										locations.forEach(l => {
+											let locationEvent = {
+												sender: event.sender,
+												postback: {
+													payload: "FACEBOOK_LOCATION",
+													data: l.payload.coordinates
+												}
+											};
+
+											facebookBot.processFacebookEvent(locationEvent);
+										});
+									}
+								}
+
+								facebookBot.processMessageEvent(event);
+							} else if (event.postback && event.postback.payload) {
+								if (event.postback.payload === "FACEBOOK_WELCOME") {
+									facebookBot.processFacebookEvent(event);
+								} else {
+									facebookBot.processMessageEvent(event);
+								}
+							}
+						});
+					}
+				});
+			}
+
+			return res.status(200).json({
+				status: "ok"
+			});
+		} catch (err) {
+			return res.status(400).json({
+				status: "error",
+				error: err
+			});
+		}
+	},
+	webhookVerification: function (req, res) {
+		let VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+
+		if (req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+			res.send(req.query['hub.challenge']);
+		} else {
+			res.send('Error, wrong validation token');
+		}
+	},
 	processIncident: function (req, res) {
 		switch (req.body.result.action) {
 			case "getIncidentStatus":
@@ -14,64 +81,6 @@ var self = {
 			case "createIncident":
 				self.createIncident(req, res);
 				break;
-		}
-	},
-	webhookEndpoint: function (req, res) {
-		let body = req.body;
-
-		// Checks this is an event from a page subscription
-		if (body.object === 'page') {
-
-			// Iterates over each entry - there may be multiple if batched
-			body.entry.forEach(function (entry) {
-
-				// Gets the body of the webhook event
-				let webhook_event = entry.messaging[0];
-				console.log(webhook_event);
-
-				// Get the sender PSID
-				let sender_psid = webhook_event.sender.id;
-				//console.log('Sender PSID: ' + sender_psid);
-
-				// Check if the event is a message or postback and
-				// pass the event to the appropriate handler function
-				if (webhook_event.message) {
-					self.handleMessage(webhook_event);
-				} else if (webhook_event.postback) {
-					self.handlePostback(webhook_event);
-				}
-			});
-
-			// Returns a '200 OK' response to all requests
-			res.status(200).send('EVENT_RECEIVED');
-		} else {
-			// Returns a '404 Not Found' if event is not from a page subscription
-			res.sendStatus(404);
-		}
-	},
-	webhookVerification: function (req, res) {
-		// Your verify token. Should be a random string.
-		let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
-		// Parse the query params
-		let mode = req.query['hub.mode'];
-		let token = req.query['hub.verify_token'];
-		let challenge = req.query['hub.challenge'];
-
-		// Checks if a token and mode is in the query string of the request
-		if (mode && token) {
-
-			// Checks the mode and token sent is correct
-			if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-				// Responds with the challenge token from the request
-				//console.log('WEBHOOK_VERIFIED');
-				res.status(200).send(challenge);
-
-			} else {
-				// Responds with '403 Forbidden' if verify tokens do not match
-				res.sendStatus(403);
-			}
 		}
 	},
 	getIncidentStatus: function (req, res) {
@@ -112,117 +121,6 @@ var self = {
 				source: '/incident/create'
 			});
 		});
-	},
-	// Handles messages events
-	handleMessage: function (webhook_event) {
-		let response;
-
-		// Check if the message contains text
-		if (webhook_event.text) {
-			// Create the payload for a basic text message
-			response = {
-				attachment: {
-					'type': 'template',
-					'payload': {
-						'template_type': 'generic',
-						'elements': [
-							{
-								'title': 'Please choose an item',
-								'image_url': 'https://diginomica.com/wp-content/uploads/2015/01/servicenow.jpeg',
-								'buttons': [
-									{
-										'type': 'postback',
-										'title': 'Create Incident',
-										'payload': 'CREATE_INCIDENT'
-									},
-									{
-										'type': 'postback',
-										'title': 'Get Incident Status',
-										'payload': 'GET_INCIDENT_STATUS'
-									}
-								]
-							}
-						]
-					}
-				}
-			}
-		}
-
-		// Sends the response message
-		self.callSendAPI(event);
-	},
-	// Handles messaging_postbacks events
-	handlePostback: function (event) {
-		let response;
-		// Get the payload for the postback
-		let payload = event.payload;
-		switch (payload) {
-			case "CREATE_INCIDENT":
-				//res = dialogflowHelper.invokeCreateIncidentEvent();
-				response = {
-				}
-				break;
-			case "GET_INCIDENT_STATUS":
-
-				break;
-		}
-		self.callSendAPI(event);
-	},
-	// Sends response messages via the Send API
-	callSendAPI: function (event) {
-		let sender = event.sender.id;
-  		let text = event.message.text;
-		// Construct the message body
-		/*let request_body = {
-			"recipient": {
-				"id": sender_psid
-			},
-			"message": response
-		}
-		// Send the HTTP request to the Messenger Platform
-		request({
-			"uri": "https://graph.facebook.com/v2.6/me/messages",
-			"qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-			"method": "POST",
-			"json": request_body
-		}, (err, res, body) => {
-			if (!err) {
-				console.log('message sent!')
-			} else {
-				console.error("Unable to send message:" + err);
-			}
-		});*/
-
-		let apiai = apiaiApp.textRequest(text, {
-			sessionId: 'tabby_cat'
-		});
-
-		apiai.on('response', (response) => {
-			console.log(response)
-			let aiText = response.result.fulfillment.speech;
-
-			request({
-				'url': 'https://graph.facebook.com/v2.6/me/messages',
-				'qs': { access_token: PAGE_ACCESS_TOKEN },
-				'method': 'POST',
-				'json': {
-					recipient: { id: sender },
-					message: { text: aiText }
-				}
-			}, (error, response) => {
-				if (error) {
-					console.log('Error sending message: ', error);
-				} else if (response.body.error) {
-					console.log('Error: ', response.body.error);
-				}
-			});
-		});
-
-		apiai.on('error', (error) => {
-			console.log(error);
-		});
-
-		apiai.end();
 	}
 }
 
